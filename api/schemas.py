f1577716569b30f7d9f756f31fc6e94160a83504
Euijoon Jung <euijoonism@gmail.com>
2025-06-21 22:01:21 +0900

    [feat] 회원가입/로그인, 그리고 약간의 수정

--- api/schemas.py
+++ api/schemas.py
@@ -1,13 +1,14 @@
 from pydantic import BaseModel, EmailStr
 from datetime import timezone
+from typing import Optional
 
 class UserCreate(BaseModel):
-    email: str
+    email: EmailStr
     password: str
     password_confirm: str
 
 class UserLogin(BaseModel):
-    email: str
+    email: EmailStr
     password: str
 
 class UserResponse(BaseModel):
@@ -18,4 +19,17 @@ class UserResponse(BaseModel):
         from_attributes = True
 
 class EmailCheck(BaseModel):
-    email: str 
+    email: EmailStr
+
+class UserAccountCreate(BaseModel):
+    api_key: str
+    api_secret: str
+    acc_no: str
+    mock: bool = False
+
+class UserAccountUpdate(BaseModel):
+    api_key: Optional[str] = None
+    api_secret: Optional[str] = None
+    acc_no: Optional[str] = None
+    mock: Optional[bool] = None
+    is_active: Optional[bool] = None 
